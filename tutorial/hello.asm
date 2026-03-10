9b4e3b12d11c1bbba54d9e80328017625c95964c
apoll011 <tiagorobotik@gmail.com>
2026-03-10 14:22:35 +0000

    Add initial project files including .gitignore and assembly source code

---
+++ tutorial/hello.asm
@@ -0,0 +1,19 @@
+global _start
+
+SECTION .data
+	hello db "Hello World!", 10
+	hello_len equ $-hello
+
+SECTION .text
+
+_start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, hello
+	mov rdx, hello_len
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
